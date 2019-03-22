a61421e39d4ce40e50eb752557a623dcce996185
Colly <kostia4567@gmail.com>
2019-03-22 20:07:06 +0200

    Parser (#2)
    * Parser
    
    Does not have any safety checks, put stuff in /source to get compiled.
    Requires source.pde to exist in /source dir.
    
    * Apply suggestions from code review
    
    Co-Authored-By: Co11y <kostia4567@gmail.com>
    
    * Apply suggestions from code review
    
    Co-Authored-By: Co11y <kostia4567@gmail.com>
    
    * Minor adjustments and fixing issues.

---
+++ parser/parser.py
@@ -0,0 +1,28 @@
+import os
+import re
+
+destination = open("../algorithmicbotany.pde", "w")
+pattern = re.compile(r'public\s+class')
+path = "../source/"
+
+def parse(fp, destination):
+  with open(path + fp, 'r') as f:
+    for line in f.readlines():
+      if pattern.match(line):
+        destination.write(re.sub('public', '', line))
+        continue
+      symbol_position = line.find("/*~") or line.find("~*/")
+      if symbol_position >= 0:
+        destination.write(line[0:symbol_position] + line[symbol_position + 4:len(line)])
+        continue
+      destination.write(line)
+
+parse('../source/source.pde', destination)
+
+for root, dirs, files in os.walk(path):
+  for fp in files:
+    if fp == "source.pde":
+      continue
+    parse(path + fp, destination)
+
+destination.close()
