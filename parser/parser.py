1ee3251a00fbf35df2f9cbbd7275c0568dbe2183
LudwigVonChesterfield <andreydell@yahoo.com>
2019-04-18 20:13:07 +0300

    Parser, Compatibility Functions module improvements.
    Introducing an """intelligibly""" designed way of being both Java and JavaScript compatible.

--- parser/parser.py
+++ parser/parser.py
@@ -5,28 +5,46 @@ destination = open("../algorithmicbotany.pde", "w")
 pattern = re.compile(r'public.*class')
 path = "../source/"
 
+
 def parse(fp, destination):
-  with open(path + fp, 'r') as f:
-    for line in f.readlines():
-      if pattern.match(line):
-        destination.write(re.sub('public ', '', line))
-        continue
-      symbol_position = line.find("/*~")
-      if symbol_position >= 0:
-        destination.write(line[0:symbol_position] + line[symbol_position + 3:len(line)])
-        continue
-      symbol_position = line.find("~*/")
-      if symbol_position >= 0:
-        destination.write(line[0:symbol_position] + line[symbol_position + 3:len(line)])
-        continue
-      destination.write(line)
+    with open(path + fp, 'r') as f:
+        for line in f.readlines():
+            if pattern.match(line):
+                destination.write(re.sub('public ', '', line))
+                continue
+            symbol_position = line.find("/*~")
+            if symbol_position >= 0:
+                destination.write(
+                    line[0:symbol_position] +
+                    line[symbol_position + 3:len(line)])
+                continue
+            symbol_position = line.find("~*/")
+            if symbol_position >= 0:
+                destination.write(
+                    line[0:symbol_position] +
+                    line[symbol_position + 3:len(line)])
+                continue
+            symbol_position = line.find("/*!-*/")
+            if symbol_position >= 0:
+                destination.write(
+                    line[0:symbol_position + 2] +
+                    line[symbol_position + 6:len(line)])
+                continue
+            symbol_position = line.find("/*-!*/")
+            if symbol_position >= 0:
+                destination.write(
+                    line[0:symbol_position] +
+                    line[symbol_position + 4:len(line)])
+                continue
+            destination.write(line)
+
 
 parse('../source/source.pde', destination)
 
 for root, dirs, files in os.walk(path):
-  for fp in files:
-    if fp == "source.pde":
-      continue
-    parse(path + fp, destination)
+    for fp in files:
+        if fp == "source.pde":
+            continue
+        parse(path + fp, destination)
 
 destination.close()
--- source/Compatibility_Functions.pde
+++ source/Compatibility_Functions.pde
@@ -1,8 +1,28 @@
+/*
+  The project while written in Java-esque language heavily depends
+  upon use of JavaScript, whose functions not declared directly by
+  Processing may differ from Java functions.
+
+  For the nefarious purposes of not having to think more about such
+  stuff this module is created.
+
+  See signum() function.
+*/
+
 public static class Compatibility_Functions
 {
   public static float signum(float f)
   {
-    return abs(f) / f;
+    // Use the pair of these to imply that code in this commented block
+    // Should be uncommented after parsing.
+    /*~
+    return Math.sign(f);
+    ~*/
+    // Use the pair of these to imply that code in this uncommented block
+    // Should be commeneted after parsing.
+    /*!-*/
+    return Math.signum(f);
+    /*-!*/
   }
 
   public static boolean isNaN(float f)
